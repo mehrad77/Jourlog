6e9526b88cde16154255d4e0981f32a4894c6cc5
Mehrad Rousta <mehrad77@gmail.com>
2020-01-14 20:33:50 +0100

    add colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 const StyledButton = styled.button``;
 
-const Button: FC = ({ children }) => {
-  return (
-    <StyledButton className=" bg-transparent hover:bg-blue-500 text-blue-200 font-semibold hover:text-white py-2 px-4 m-2 border border-blue-500 hover:border-transparent rounded">
-      {children}
-    </StyledButton>
-  );
+interface IButton {
+  variant?: 'primary' | 'secondary';
+}
+
+const Button: FC<IButton> = ({ children, variant = 'primary' }) => {
+  return <StyledButton className={`btn btn-${variant}`}>{children}</StyledButton>;
 };
 
 export { Button };
