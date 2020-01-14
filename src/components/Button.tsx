f115d005f3e734232c1ac042b20441c9bf26fafe
Mehrad Rousta <mehrad77@gmail.com>
2020-01-14 18:17:37 +0100

    change button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,14 @@
+import React, { FC } from 'react';
 import styled from 'styled-components';
-import curriedDarken from 'polished/lib/color/darken';
 
-export const Button: any = styled.button`
-  background: ${props => props.theme.colors.primary};
-  border: none;
-  display: inline-flex;
-  align-items: center;
-  margin: 0 0.5rem;
-  border-radius: ${(props: any) => (props.big ? '1.5rem' : '1rem')};
-  font-size: ${(props: any) => (props.big ? '1.2rem' : '1rem')};
-  color: white;
-  padding: ${(props: any) => (props.big ? '0.35rem 1.6rem' : '0.25rem 1.5rem')};
-  transition: all ${(props: any) => props.theme.transitions.normal};
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  &:hover {
-    background: ${(props: any) => curriedDarken(0.3, props.theme.colors.primary)};
-    cursor: pointer;
-    transform: translateY(-2px);
-  }
-  &:focus {
-    outline: none;
-  }
-  svg {
-    width: 32px;
-    height: 32px;
-    margin-right: 0.75rem;
-    fill: white;
-  }
-`;
+const StyledButton = styled.button``;
+
+const Button: FC = ({ children }) => {
+  return (
+    <StyledButton className=" bg-transparent hover:bg-blue-500 text-blue-200 font-semibold hover:text-white py-2 px-4 m-2 border border-blue-500 hover:border-transparent rounded">
+      {children}
+    </StyledButton>
+  );
+};
+
+export { Button };
