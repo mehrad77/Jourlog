55379fba6ceb0cdd927cc2cd7ac5d61ccbb618f5
Mehrad Rousta <mehrad77@gmail.com>
2020-01-15 12:37:38 +0100

    better design and structure for works page

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,51 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { FC } from 'react';
 
-export const Card: any = styled.figure`
-  border-width: 1px 1px 1px 1px;
-  border-color: rgba(232, 232, 232, 1);
-  border-radius: 3;
-  border-style: solid;
-  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.25); /* hover styles */
-  display: inline-block;
-  background: #fff;
-  border-radius: 2px;
-  transform: rotate(-10deg);
-  transition: transform 0.8s;
+interface IWork {
+  title: string;
+  link: string;
+  image?: string;
+  description: string;
+}
 
-  img {
-    margin-bottom: 0px;
-    min-height: 250px;
-    max-height: 300px;
-  }
-
-  figcaption {
-    text-align: center;
-    font-size: 30px;
-  }
-
-  :nth-child(2) {
-    transform: rotate(23deg);
-    transition: transform 0.2s;
-  }
-
-  :nth-child(3) {
-    transform: rotate(-2deg);
-    transition: transform 0.2s;
-  }
-
-  :hover {
-    transform: rotate(0deg);
-    transition: transform 0.2s;
-  }
-`;
-
-const Work = ({ img, children, link }: { img?: string; children?: any; link?: string }) => {
+const Work: FC<IWork> = ({ image = 'http://placehold.it/250x250.jpg', link, title, description }) => {
   return (
-    <Card>
-      <img src={img ? img : 'http://placehold.it/250x250.jpg'} />
-      <figcaption>{children ? children : 'A TV I saw once'}</figcaption>
-    </Card>
+    <div className="block flex flex-row w-full mb-8 bg-backWhite-opaque rounded-md overflow-hidden shadow-md">
+      <img className="w-3/12 relative object-cover" src={image} />
+      <figcaption className="w-9/12 py-2 px-4">
+        <h3 className="text-2xl text-foreText-heading">{title}</h3>
+        <p className="text-foreText-paragraph text-sm">{description}</p>
+        <a
+          href={link}
+          className="px-4 py-2 align-baseline bg-primary-600 hover:text-white hover:shadow-md text-white font-bold float-right"
+        >
+          Visit
+        </a>
+      </figcaption>
+    </div>
   );
 };
 
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { FC } from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 import { Layout, Wrapper, Header, Content, SectionTitle } from '../components';
@@ -7,30 +6,34 @@ import config from '../../config/SiteConfig';
 import PageProps from '../models/PageProps';
 import Work from '../components/Work';
 
-const WorkWrapper = styled.div`
-  text-align: center;
-  padding-top: 15px;
-`;
-
-export default class ContactPage extends React.Component<PageProps> {
-  public render() {
-    return (
-      <Layout>
-        <Helmet title={`Contact | ${config.siteTitle}`} />
-        <Header banner={'/assets/bg/1.jpg'}>
-          <Link to="/">{config.siteTitle}</Link>
-          <SectionTitle uppercase={true}>Mehrad's Works</SectionTitle>
-        </Header>
-        <Wrapper>
-          <Content>
-            <p>Sometimes I do cool stuf, sometimes I co-oprate with others to make cool stuf, and sometimes I admire cool stufs.</p>
-            <WorkWrapper>
-              <Work img="https://secureimg.stitcher.com/feedimagesplain328/210171.jpg">Jigsaw Podcast </Work>
-              <Work img="https://sinatapodcast.com/photo.jpg">Sinata Podcast </Work>
-            </WorkWrapper>
-          </Content>
-        </Wrapper>
-      </Layout>
-    );
-  }
+interface IWork {
+  title: string;
+  link: string;
+  image: string;
+  description: string;
 }
+
+const WorksPage: FC<PageProps> = () => {
+  const data: IWork[] = require('../../static/works.json');
+  return (
+    <Layout>
+      <Helmet title={`Contact | ${config.siteTitle}`} />
+      <Header banner={'/assets/bg/1.jpg'}>
+        <Link to="/">{config.siteTitle}</Link>
+        <SectionTitle>Mehrad's Works</SectionTitle>
+      </Header>
+      <Wrapper>
+        <Content>
+          <p>Sometimes I do cool stuf, sometimes I co-oprate with others to make cool stuf, and sometimes I admire cool stufs.</p>
+          <div className="flex flex-col my-4">
+            {data.map(props => (
+              <Work {...props} />
+            ))}
+          </div>
+        </Content>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default WorksPage;
